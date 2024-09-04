8c9f9b14a19d3429f5d7ff4ff91367397e724c46
Rocky14683 <rocky10208@gmail.com>
2024-09-04 16:15:16 -0400

    init commit

---
+++ src/main.cpp
@@ -0,0 +1,45 @@
+#include "main.h"
+
+
+void initialize() {
+    pros::lcd::initialize();
+    pros::lcd::set_text(1, "Hello PROS User!");
+
+    pros::lcd::register_btn1_cb(on_center_button);
+}
+
+void disabled() {}
+
+
+void competition_initialize() {}
+
+
+void autonomous() {}
+
+
+pros::Controller controller(pros::E_CONTROLLER_MASTER);
+pros::MotorGroup left_mtr({});
+pros::MotorGroup right_mtr({});
+
+void display_task() {
+    pros::Task task([]{
+        while(true) {
+            // display motor position
+
+            // display motor velocity
+            pros::delay(100);// delay for context switching
+        }
+    });
+}
+
+void opcontrol() {
+    // spawn display task
+
+    // opcontrol while loop
+    while(true) {
+        // motor control code
+
+
+        pros::delay(10);// delay for context switching
+    }
+}
